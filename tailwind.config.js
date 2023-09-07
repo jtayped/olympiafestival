ca3707798e78eb123905e4810c3570c478556da4
Joel Taylor <jtayped@gmail.com>
2023-09-07 10:07:10 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        text: "#080802",
+        background: "#fbfbf3",
+        primary: "#fbfef7",
+        secondary: "#79f5fa",
+        accent: "#bc93e7",
+      },
+    },
+  },
+  plugins: [],
+};
