e3935286cb37e28e331fc83bc773f28660ed1a53
Joel Taylor <jtayped@gmail.com>
2023-09-07 10:13:59 +0200

    containers created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./containers";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path="/" element="Home"/>
+          <Route path="/" element="Home" />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+// React util
+import React from "react";
+
+// Containers
+import { Home } from "../containers";
+
+const Home = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default Home;
