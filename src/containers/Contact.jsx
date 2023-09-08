4828fad4cf7a8b518f51a71010e20b345e834418
Joel Taylor <jtayped@gmail.com>
2023-09-08 20:30:12 +0200

    form functionallity

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,81 +1,125 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { useTranslation } from "react-i18next";
 
+// EmailJS
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
+  const form = useRef();
   const [t, i18n] = useTranslation("global");
-  function handleSubmit(event) {
+
+  const [error, setError] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [reason, setReason] = useState(
+    t("contact.inputs.reason.reasons.0.value")
+  );
+  const [message, setMessage] = useState("");
+
+  function send(event) {
     event.preventDefault();
+
+    // Error handling
+    if (name) {
+      setError("Ple");
+    }
+
+    emailjs
+      .sendForm(
+        "service_5wfmyl9",
+        "template_2fh850m",
+        form.current,
+        "B7hcLfaU_FcnlgJPM"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    console.log(name, email, number, reason, message);
   }
   return (
     <div className="flex items-center justify-center w-full p-10">
-      <div className="flex flex-col w-full max-w-[900px]">
+      <div className="flex flex-col w-full max-w-[800px]">
         <h2 className="font-accent text-3xl sm:text-5xl">
           {t("contact.title")}
         </h2>
-        <form className="mt-2" action={(e) => handleSubmit(e)}>
+        <form ref={form} className="mt-2" action={(e) => send(e)}>
           <div className="flex flex-col gap-2 md:gap-4 md:grid md:grid-cols-2">
             <div className="flex flex-col">
-              <label htmlFor={t("contact.inputs.name.name")}>
-                {t("contact.inputs.name.name")}
-              </label>
+              <label htmlFor="from_name">{t("contact.inputs.name.name")}</label>
               <input
                 type="text"
-                name={t("contact.inputs.name.name")}
+                onChange={(e) => setName(e.target.value)}
+                name="from_name"
                 placeholder={t("contact.inputs.name.placeholder")}
                 className="contact-input"
               />
             </div>
             <div className="flex flex-col">
-              <label htmlFor={t("contact.inputs.email.name")}>
-                {t("contact.inputs.email.name")}
-              </label>
+              <label htmlFor="email">{t("contact.inputs.email.name")}</label>
               <input
                 type="text"
-                name={t("contact.inputs.email.name")}
+                onChange={(e) => setEmail(e.target.value)}
+                name="email"
                 placeholder={t("contact.inputs.email.placeholder")}
                 className="contact-input"
               />
             </div>
             <div className="flex-col hidden md:flex">
-              <label htmlFor={t("contact.inputs.number.name")}>
-                {t("contact.inputs.number.name")}
-              </label>
+              <label htmlFor="number">{t("contact.inputs.number.name")}</label>
               <input
                 type="text"
-                name={t("contact.inputs.number.name")}
+                onChange={(e) => setNumber(e.target.value)}
+                name="number"
                 placeholder={t("contact.inputs.number.placeholder")}
                 className="contact-input"
               />
             </div>
             <div className="flex flex-col">
-              <label htmlFor={t("contact.inputs.reason.name")}>
-                {t("contact.inputs.reason.name")}
-              </label>
+              <label htmlFor="reason">{t("contact.inputs.reason.name")}</label>
               <select
-                name={t("contact.inputs.reason.name")}
-                className="contact-input"
+                onChange={(e) => setReason(e.target.value)}
+                name="reason"
+                className="contact-input px-4"
               >
                 {t("contact.inputs.reason.reasons", {
                   returnObjects: true,
                 }).map((reason, index) => (
-                  <option key={index} value="reason">
-                    {reason}
+                  <option
+                    className="text-black"
+                    key={index}
+                    value={reason.value}
+                  >
+                    {reason.label}
                   </option>
                 ))}
               </select>
             </div>
             <div className="flex flex-col col-span-2">
-              <label htmlFor={t("contact.inputs.message.name")}>
+              <label htmlFor="message">
                 {t("contact.inputs.message.name")}
               </label>
               <textarea
-                name={t("contact.inputs.message.name")}
-                cols="30"
+                onChange={(e) => setMessage(e.target.value)}
+                name="message"
                 rows="10"
                 className="contact-input"
                 placeholder={t("contact.inputs.message.placeholder")}
               ></textarea>
             </div>
+            {error ? <p className="text-red-400"></p> : null}
+            <button
+              type="submit"
+              className="bg-white text-black font-accent py-1 text-lg"
+            >
+              {t("contact.submitBtn")}
+            </button>
           </div>
         </form>
       </div>
